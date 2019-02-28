2f8a4a94a03ba96212543512faada9022a04280a
Vinicius Alexandre <vns140@hotmail.com>
2019-02-28 20:04:46 -0300

    metodo post

--- src/main/java/com/viniciussaraiva/workshopmongo/resources/UserResource.java
+++ src/main/java/com/viniciussaraiva/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.viniciussaraiva.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.viniciussaraiva.workshopmongo.domain.User;
 import com.viniciussaraiva.workshopmongo.dto.UserDTO;
 import com.viniciussaraiva.workshopmongo.services.UserService;
@@ -33,5 +36,13 @@ public class UserResource {
 		User user = userService.findById(id);
 		return ResponseEntity.ok(new UserDTO(user));
 	}
-
+	
+	@PostMapping()
+	public ResponseEntity<Void> Post(@RequestBody UserDTO userDTO) {
+		User user = new User(userDTO.getId(),userDTO.getEmail(),userDTO.getName());
+		userService.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
 }
--- src/main/java/com/viniciussaraiva/workshopmongo/services/UserService.java
+++ src/main/java/com/viniciussaraiva/workshopmongo/services/UserService.java
@@ -27,4 +27,8 @@ public class UserService {
 		}
 		return user.get();
 	}
+	
+	public User insert(User user) {
+		return userRepository.insert(user);
+	}
 }
